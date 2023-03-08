cd80e8eb9eb4485a9785d8a87a6defa63d90e881
mgvts <89970331+mgvts@users.noreply.github.com>
2023-03-08 23:00:33 +0300

    add some changes to grad now like vector

--- lab1/examples/2.1.py
+++ lab1/examples/2.1.py
@@ -16,7 +16,7 @@ f = Func(1, stringFunc)
 a = -10
 b = 10
 eps = 0.001
-#must be from (0, 2eps)
+# must be from (0, 2eps)
 delta = 0.0015
 N = math.ceil(calc_min_iterations())
 print(f"{N =}")
@@ -25,26 +25,26 @@ x1 = (a + b - delta) / 2
 x2 = (a + b + delta) / 2
 t1 = (b - x1) / (b - a)
 t2 = (x2 - a) / (b - a)
-#t1 and t2 must be lim = 1/2
+# t1 and t2 must be lim = 1/2
 print(f"{t1 = } {t2 = }")
 for i in range(N):
     print(f"{x1 = } {x2 = } {f.eval([('x0', x1)]) = } {f.eval([('x0', x2)]) = }")
-    #1 step
+    # 1 step
     x1 = (a + b - delta) / 2
     x2 = (a + b + delta) / 2
 
-    #2 step
+    # 2 step
     if f.eval([('x0', x1)]) <= f.eval([('x0', x2)]):
         b = x2
     else:
         a = x1
 
-    #3 step
+    # 3 step
     eps_i = (b - a) / 2
     if eps_i <= eps:
         break
 
-#4 step
+# 4 step
 X = (a + b) / 2
 print(X)
-#X is root of this function
+# X is root of this function
