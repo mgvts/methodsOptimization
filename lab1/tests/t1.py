dd808b45407f917bcfb59c068f8212e6f02af452
Владимир Назаров <vova24848@gmail.com>
2023-03-08 22:31:17 +0300

    add simple tests for gradient

--- lab1/tests/t1.py
+++ lab1/tests/t1.py
@@ -1,6 +1,13 @@
 import unittest
 
 
+# func, point, ans
+tests_grad_in_point = [
+    ('x0 + ln(x2^2 + x1^2)', [2, 1, 1], [1, 1, 1]), # https://xn--24-6kcaa2awqnc8dd.xn--p1ai/gradient-funkcii-v-tochke.html
+    ('x0^2 + 2*x1*x2 + x2^2 + x3^2', [1, 1, 1], [4, 4, 2]), # https://math.semestr.ru/math/grad.php
+    ('x0^2 + 2*x1*x2 + x2^2 + x3^2', [2, -1, 0], [2, 1, 0])
+]
+
 tests = [
     ('x1^2', [0]),
     ('x1^2 + 10', [0]),
