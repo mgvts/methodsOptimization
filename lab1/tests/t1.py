2eed3b9ea6a149590b9f2f3b642af2a6462fb0b4
mgvts <89970331+mgvts@users.noreply.github.com>
2023-03-08 23:30:06 +0300

    add some questions

--- lab1/tests/t1.py
+++ lab1/tests/t1.py
@@ -7,6 +7,7 @@ tests_grad_in_point = [
     (3, 'x0 + ln(x2^2 + x1^2)', [2, 1, 1], [1, 1, 1]), # https://xn--24-6kcaa2awqnc8dd.xn--p1ai/gradient-funkcii-v-tochke.html
     (3, 'x0^2 + 2*x0*x1 + x1^2 + x2^2', [1, 1, 1], [4, 4, 2]), # https://math.semestr.ru/math/grad.php
     (3, 'x0^2 + 2*x0*x1 + x1^2 + x2^2', [2, -1, 0], [2, 1, 0])
+    # a очно праивльный тест? из калькулятора  grad(u)=(2·x+2·y)·i + (2·x+2·y)·j + 2·z·k
 ]
 
 tests = [
@@ -22,6 +23,7 @@ tests = [
     ('x1^2 * (x2+2)^2 - 10', [0, -2])
 ]
 
+
 def to_args(t, n):
     return [(f"x{i}", t[i]) for i in range(n)]
 
@@ -30,4 +32,5 @@ class TestRandomGradient(unittest.TestCase):
     def test_readable(self):
         for n, s, m, a in tests_grad_in_point:
             f = Func(n, s)
+            print(s)
             self.assertEqual(list(f.grad(to_args(m, n))), a)
