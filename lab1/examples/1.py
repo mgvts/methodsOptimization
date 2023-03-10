1ec675ac63be4f9422dd96e0717fedd7e4d14389
Владимир Назаров <vova24848@gmail.com>
2023-03-10 15:26:43 +0300

    grad bug

--- lab1/examples/1.py
+++ lab1/examples/1.py
@@ -1,33 +1,12 @@
 import random
 
+from lab1.grad import grad_down
 from lab1.tools import Func
 from time import sleep
 import sympy as sp
 
 n = 2
-stringFunc = "x0^2 + x1^2 + 10"
-f = Func(2, stringFunc)
-eps = 0.01
-alpha = 0.1
-# именно столько скобок [[]]
-x = sp.Matrix([[random.randint(0, 10) for i in range(n)]])
-print(x)
+stringFunc = "x0^4 + x1^2 - 10"
 
-
-def to_args(t):
-    return [(f"x{i}", t[i]) for i in range(n)]
-
-
-while True:
-    # ||∇f(x)|| < ε
-    if f.metric_of_gradient_in_point(to_args(x)) < eps:
-        break
-
-    y = x - alpha * f.grad(to_args(x))
-    if f.eval(to_args(y)) < f.eval(to_args(x)):
-        x = y
-
-        # alpha = const in 1st task
-        # alpha = alpha / 2
-print(x)
-print(f.eval(to_args(x)))
+# если x = Matrix([[-10, 0]]) , то бесконечный цикл
+print(grad_down(n, stringFunc))
--- lab1/tests/t1.py
+++ lab1/tests/t1.py
@@ -1,26 +1,28 @@
 import unittest
 
+from lab1.grad import grad_down
 from lab1.tools import Func
 
 # func, point, ans
 tests_grad_in_point = [
-    (3, 'x0 + ln(x2^2 + x1^2)', [2, 1, 1], [1, 1, 1]), # https://xn--24-6kcaa2awqnc8dd.xn--p1ai/gradient-funkcii-v-tochke.html
-    (3, 'x0^2 + 2*x0*x1 + x1^2 + x2^2', [1, 1, 1], [4, 4, 2]), # https://math.semestr.ru/math/grad.php
+    (3, 'x0 + ln(x2^2 + x1^2)', [2, 1, 1], [1, 1, 1]),
+    # https://xn--24-6kcaa2awqnc8dd.xn--p1ai/gradient-funkcii-v-tochke.html
+    (3, 'x0^2 + 2*x0*x1 + x1^2 + x2^2', [1, 1, 1], [4, 4, 2]),  # https://math.semestr.ru/math/grad.php
     (3, 'x0^2 + 2*x0*x1 + x1^2 + x2^2', [2, -1, 0], [2, 1, 0])
     # a очно праивльный тест? из калькулятора  grad(u)=(2·x+2·y)·i + (2·x+2·y)·j + 2·z·k
 ]
 
-tests = [
-    ('x1^2', [0]),
-    ('x1^2 + 10', [0]),
-    ('x1^4 + 10', [0]),
-    ('x1^6 - 10', [0]),
-    ('x1^6 + x2^2 - 10', [0, 0]),
-    ('x1^6 + (x2 - 1)^2 - 10', [0, 1]),
-    ('(x1 + 8)^6 + (x2 - 1)^2 - 10', [-8, 1]),
-    ('(x1 + 8)^6 + (x2 - 1)^2 - 10', [-8, 1]),
-    ('x1^2 * x2^2 - 10', [0, 0]),
-    ('x1^2 * (x2+2)^2 - 10', [0, -2])
+tests_s = [
+    (1, 'x0^2', [0]),
+    (1, 'x0^2 + 10', [0]),
+    (1, 'x0^4 + 10', [0]),
+    # (1, 'x0^6 - 10', [0]), разве нет минимума?
+    # (2, 'x0^4 + x1^2 - 10', [0, 0]),
+    # (2, 'x0^6 + (x1 - 1)^2 - 10', [0, 1]),
+    # (2, '(x0 + 8)^6 + (x1 - 1)^2 - 10', [-8, 1]),
+    # (2, '(x0 + 8)^6 + (x1 - 1)^2 - 10', [-8, 1]),
+    # (2, 'x0^2 * x1^2 - 10', [0, 0]),
+    # (2, 'x0^2 * (x1+2)^2 - 10', [0, -2])
 ]
 
 
@@ -32,5 +34,8 @@ class TestRandomGradient(unittest.TestCase):
     def test_readable(self):
         for n, s, m, a in tests_grad_in_point:
             f = Func(n, s)
-            print(s)
             self.assertEqual(list(f.grad(to_args(m, n))), a)
+
+    def test_readable_grad(self):
+        for n, s, a in tests_s:
+            print(grad_down(n, s))
