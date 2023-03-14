0b8e11243a0ec687282388f2702d27e33322be1d
mgvts <89970331+mgvts@users.noreply.github.com>
2023-03-14 19:09:12 +0300

    add analize to graph

--- lab1/analyze/graph.py
+++ lab1/analyze/graph.py
@@ -7,9 +7,34 @@ from sympy import plot_implicit, symbols
 from sympy.plotting.plot import MatplotlibBackend, Plot
 
 from lab1.grad import grad_down_dichotomy, grad_down
-from lab1.tools import Func
+from lab1.tools import Func, get_metric2
 
 mpl.use('TkAgg')
+
+
+def analize(start_point, out_from_const, out_from_dichotomy):
+    print("start_point", end="\t")
+    print(start_point)
+    print("grad_down")
+    print(out_from_const)
+    print("grad_down_dichotomy")
+    print(out_from_dichotomy)
+    print(f"last points: статичный{out_from_const.points[-1].values()} "
+          f"дихотомия{out_from_dichotomy.points[-1].values()} "
+          f"equals: {out_from_const.points[-1].values() == out_from_dichotomy.points[-1].values()}", sep="\n")
+    print()
+    print("разница в значениях на итерациях")
+    print("i       static              dichotomy")
+    for i in range(len(out_from_const.points)):
+        print(f"{i = }")
+        print(f"точки {out_from_const.points[i].values()} {out_from_dichotomy.points[i].values()}")
+        print(f"alpha {out_from_const.alpha[i]} {out_from_dichotomy.alpha[i]}")
+        print(f"метрика 2 от стартовой до данной точки")
+        print(get_metric2(start_point - out_from_const.points[i]), get_metric2(start_point - out_from_dichotomy.points[i]))
+        print(f"метрика 2 от коннечной до данной точки")
+        print(get_metric2(out_from_const.points[-1] - out_from_const.points[i]), get_metric2(out_from_dichotomy.points[-1] - out_from_dichotomy.points[i]))
+        print()
+        print()
 
 
 def get_sympy_subplots(plot: Plot):
@@ -59,7 +84,7 @@ f = func.f
 func2 = Func(n, stringFunc)
 f2 = func2.f
 x, y = symbols('x0, x1')
-start_point = sp.Matrix([[randint(-5, 5) for i in range(n)]])
+start_point = sp.Matrix([[-1000 for i in range(n)]])
 
 x_range = [-10, 10]
 y_range = [-10, 10]
@@ -68,6 +93,7 @@ p = plot_implicit(f, show=False, points=10, x_var=(x, *x_range), y_var=(y, *y_ra
 plt1 = get_sympy_subplots(p)
 
 x2 = grad_down_dichotomy(n, stringFunc, start_point, max_inter=10)
+out_from_const = x2
 for i in range(1, len(x2.points)):
     x1, y1 = [x2.points[i - 1][0], x2.points[i][0]], [x2.points[i - 1][1], x2.points[i][1]]
     line = plt1.gca().plot(x1, y1, marker='o', ms=1)
@@ -79,7 +105,8 @@ for i in range(1, len(x2.points)):
 
 p2 = plot_implicit(f2, show=False, points=10, x_var=(x, -10, 10), y_var=(y, -10, 10))
 plt2 = get_sympy_subplots(p2)
-x2 = grad_down(n, stringFunc, start_point, max_inter=2000, alpha=0.01)
+x2 = grad_down(n, stringFunc, start_point, max_inter=10, alpha=0.3)
+out_from_dichotomy = x2
 for i in range(1, len(x2.points)):
     x1, y1 = [x2.points[i - 1][0], x2.points[i][0]], [x2.points[i - 1][1], x2.points[i][1]]
     line2 = plt2.plot(x1, y1, marker='o', ms=1)
@@ -88,6 +115,8 @@ for i in range(1, len(x2.points)):
     except IndexError:
         # small dist -> not line, point
         pass
+analize(start_point, out_from_const, out_from_dichotomy)
 
 plt1.show()
 plt2.show()
+
