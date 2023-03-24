fbcbbb92bd7682fdfdf352e79d057e519040527a
Владимир Назаров <vova24848@gmail.com>
2023-03-24 22:24:06 +0300

    add fast func

--- lab1/fast_grad.py
+++ lab1/fast_grad.py
@@ -18,7 +18,7 @@ class OutputDTO:
     dichotomy_count: list[int]
 
 
-eps_CONST = 0.0001
+eps_CONST = 0.000001
 alpha_CONST = 0.1
 max_INTER = 1000
 
@@ -59,7 +59,7 @@ def grad_down(func: FastQFunc,
         metr = get_metric3(func.grad(y) - func.grad(x)) # todo change metric
 
         out.points.append(x)
-        out.points_with_floats.append(x)
+        out.points_with_floats.append(x.transpose().tolist()[0])
         out.alpha.append(alpha)
         out.metrics.append(metr)
         out.iter += 1
@@ -124,7 +124,7 @@ def grad_down_dichotomy(func: FastQFunc,
     :param eps: eps in gradient method
     :return: OutputDTO, dataclass for analyze
     """
-    x = np.matrix(start_point)
+    x = np.matrix(start_point).transpose()
 
     out = OutputDTO(
         points=[],
@@ -145,8 +145,85 @@ def grad_down_dichotomy(func: FastQFunc,
         metr = get_metric3(func.grad(y) - func.grad(x))
 
         out.dichotomy_count.append(count)
-        out.points.append(x)
-        out.points_with_floats.append(x.values())
+        out.points.append(x.transpose().tolist()[0])
+        out.points_with_floats.append(x)
+        out.alpha.append(alpha)
+        out.metrics.append(metr)
+        out.iter += 1
+
+        # ||∇f(x)|| < ε
+        if metr < eps:
+            break
+
+        if func.eval(y) < func.eval(x):
+            x = y
+        if len(out.points) > max_inter:
+            out.was_broken = True
+            return out
+    return out
+
+
+
+def parse_eval(f: FastQFunc):
+    return lambda x: f.eval(to_args(x, f.get_n()))
+
+
+def wolfe_conditions(func: FastQFunc, x: np.matrix, alpha: float, c1=0.01, c2=0.99):
+    grad_fx = func.grad(x)
+    p = func.grad(x)
+    fx_alpha = func.eval(x - alpha * p)
+    grad_fx_alpha = func.grad(x - alpha * p)
+    cond1 = fx_alpha <= func.eval(x) + c1 * alpha * (grad_fx.transpose() * p)
+    cond2 = (grad_fx_alpha.transpose() * p) >= c2 * (grad_fx_alpha.transpose() * p)
+    return cond1 and cond2
+
+
+# todo
+def find_alpha_with_wolfe(f: FastQFunc,
+                          start_point: np.matrix,
+                          c1=0.001, c2=0.99):
+    alpha = 1
+    while not wolfe_conditions(f, start_point, alpha, c1=c1, c2=c2):
+        alpha *= 0.5
+    return alpha
+
+def grad_down_wolfe(func: FastQFunc,
+                    start_point: [float],
+                    eps=eps_CONST,
+                    max_inter=max_INTER) -> OutputDTO:
+    """
+    :param max_inter: max interaction should be run
+    :param n: how many variables in function
+    :param string_func: string of Function
+    :param start_point: Start point like sp.Matrix([[1,1]])
+    :param function_alpha: function to changed  alpha
+    :param eps: eps in gradient method
+    :return: OutputDTO, dataclass for analyze
+    """
+    x = np.matrix(start_point).transpose()
+
+    out = OutputDTO(
+        points=[],
+        points_with_floats=[],
+        alpha=[],
+        string_func="none",
+        n=func.n,
+        was_broken=False,
+        eps=eps,
+        metrics=[],
+        iter=0,
+        dichotomy_count = []
+    )
+
+    while True:
+        alpha = find_alpha_with_wolfe(func, x)
+        # alpha = dichotomy(lambda a: f.eval(to_args(x - a * f.grad(to_args(x, n)).evalf(), n)))
+        y = x - alpha * func.grad(x)
+        metr = get_metric3(func.grad(y) - func.grad(x))
+        print(alpha, metr, func.grad(y), func.grad(x))
+
+        out.points.append(x.transpose().tolist()[0])
+        out.points_with_floats.append(x)
         out.alpha.append(alpha)
         out.metrics.append(metr)
         out.iter += 1
