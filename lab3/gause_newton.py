7a7dbb4c31b5431b43c1b32ecd5953b3098837b8
Phoenix <vova24848@gmail.com>
2023-06-17 18:30:21 +0300

    compl example for method gause-newton

--- lab3/gause_newton.py
+++ lab3/gause_newton.py
@@ -3,29 +3,45 @@ from lab3.util import derivative
 
 R = np.matrix(
     [
-        [1.1345, 0.725, 3.734],
-        [1.488, 57.547, 1.512],
-        [2.3457, 0.74, 1.321],
-        [1.3754, 0.457, 21.123]
+        [0.038, 0.050],
+        [0.194, 0.127],
+        [0.425, 0.094],
+        [0.626, 0.2122],
+        [1.253, 0.2729],
+        [2.500, 0.2665],
+        [3.740, 0.3317],
     ]
 )
 
-B = np.matrix([1.999, 1.0, 5.1223]).T
-# print(R * B)
+B = np.matrix([0.9, 0.2]).T
 
 for i in range(10):
     J = []
+    b1 = B[0, 0]
+    b2 = B[1, 0]
+    RB = []
+    eps = 0
     for u in R:
-        f = lambda i: (u * i)[0, 0]
+        x = u[0, 0]
 
-        J.append([derivative(f, p[0, 0]) for p in B])
+        J.append(
+            [
+                -x / (b2 + x),
+                (b1 * x) / (b2 + x) ** 2
+            ]
+        )
+        s = u[0, 1] - (b1 * x) / (b2 + x)
+        RB.append(s)
+        eps += (u[0, 1] - s) ** 2
+
+    print(eps)
+
     J = np.matrix(J)
-
+    RB = np.matrix(RB).T
     r = np.linalg.matrix_rank(J)
     if r < J.shape[1]:
         print('failed: rg(J) =', r)
         break
 
-    print(J)
-    B = B - np.linalg.inv(J.T * J) * J.T * (R * B)
-    print(B)
+    B = B - np.linalg.inv(J.T * J) * J.T * RB
+    print()
