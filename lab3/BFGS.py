2507a56e5527b9e0f63633c25bde387a7672efe6
mgvts <89970331+mgvts@users.noreply.github.com>
2023-06-18 16:41:39 +0300

    change bfgs and add prerealize of lbfgs

--- lab3/BFGS.py
+++ lab3/BFGS.py
@@ -1,4 +1,4 @@
-from collections import Callable
+from collections.abc import Callable
 
 import numpy as np
 
@@ -21,17 +21,17 @@ def bfgs(f: Callable[[np.array], float],
             break
         # changing p = - H_k *∇ f_k
         # same as p = -np.dot(H, grad)
-        p = -H @ grad
-        # alpha finding with well known Wolfe search
-        alpha = line_search(f, grad_f, x, p)
+        p = - H @ grad
+        # alpha finding with well known line search with Wolfe conditions
 
+        alpha = line_search(f, grad_f, x, p)
         # немного хитро, но присмотритесь, это то что надо
         # (x_new = x + alpha*p; s = x_new - x)
         s = alpha * p
         x_new = x + s
 
         y = grad_f(x_new) - grad
-        rho = 1 / np.dot(y, s)
+        rho = 1 / (y @ s)
         # в отчёте будут красивые картинки, просто поверьте, что это работает
         # np.outer(s, y) = s @ (y.T)
         A = np.eye(n) - rho * np.outer(s, y)
@@ -46,24 +46,76 @@ def line_search(f: Callable[[np.array], float],
                 x: np.array,
                 p: np.array) -> float:
     alpha = 1
-    c1 = 1e-4
+    c1 = 0.001
     c2 = 0.9
 
     while not (get_cond1(f, grad_f, x, alpha, p, c1=c1) and
                get_cond2(grad_f, x, alpha, p, c2=c2)):
         alpha *= 0.5
-
+        if alpha == 0:
+            raise AssertionError("in line search alpha equals zero")
     return alpha
 
 
-# also H@p same as np.dot(H, p)
 def get_cond1(f: Callable[[np.array], float],
               grad_f: Callable[[np.array], np.array],
-              x: np.array, alpha: float, p: np.array, c1: float) -> bool:
+              x: np.array,
+              alpha: float,
+              p: np.array, c1: float) -> bool:
     return f(x + alpha * p) <= f(x) + c1 * alpha * (grad_f(x).T @ p)
 
 
 # also H@p same as np.dot(H, p)
 def get_cond2(grad_f: Callable[[np.array], np.array],
               x: np.array, alpha: float, p: np.array, c2: float) -> bool:
-    return grad_f(x - alpha * p).T @ p >= c2 * (grad_f(x).T @ p)
+    return grad_f(x + alpha * p).T @ p >= c2 * (grad_f(x).T @ p)
+
+
+# пока хуй знает, спасил с гпт вроде выглядит нормально
+def lbfgs(f: Callable[[np.array], float],
+          grad_f: Callable[[np.array], np.array],
+          x0: np.array,
+          eps=1e-6,
+          max_iter=10,
+          m=10) -> np.array:
+    n = len(x0)
+    H = np.eye(n)
+    x = x0
+    s_list = []
+    y_list = []
+    for i in range(max_iter):
+        grad = grad_f(x)
+        if np.linalg.norm(grad) < eps:
+            break
+        p = - H @ grad
+
+        alpha = line_search(f, grad_f, x, p)
+        s = alpha * p
+        x_new = x + s
+
+        y = grad_f(x_new) - grad
+        rho = 1 / (y @ s)
+
+        # добавляем векторы s и y в очередь
+        if len(s_list) == m:
+            s_list.pop(0)
+            y_list.pop(0)
+        s_list.append(s)
+        y_list.append(y)
+
+        # вычисляем q и r
+        q = grad.copy()
+        alpha_list = []
+        for s_i, y_i in zip(reversed(s_list), reversed(y_list)):
+            alpha_i = rho * s_i @ q
+            alpha_list.append(alpha_i)
+            q -= alpha_i * y_i
+        r = H @ q
+        for s_i, y_i, alpha_i in zip(s_list, y_list, reversed(alpha_list)):
+            beta_i = rho * y_i @ r
+            r += (alpha_i - beta_i) * s_i
+
+        H = np.outer(s, y) / (y @ y) + np.diag(r)
+
+        x = x_new
+    return x
--- lab3/examples/bfgs_example.py
+++ lab3/examples/bfgs_example.py
@@ -3,9 +3,41 @@ import numpy as np
 from lab3.BFGS import bfgs
 
 
-f = lambda x: sum([i ** 2 for i in x])
-grad_f = lambda x: np.array([2 * i for i in x])
+def bfgs_example1():
+    # f(x) =x1^2 + x2^2 + x3^2 + ...
+    f = lambda x: sum([i ** 2 for i in x])
+    grad_f = lambda x: np.array([2 * i for i in x])
 
-n = 3
-x0 = np.array([1 for i in range(n)]).T
-print(bfgs(f, grad_f, x0))
+    n = 3
+    x0 = np.array([1 for i in range(n)]).T
+    print(bfgs(f, grad_f, x0))
+
+
+def bfgs_example2():
+    def f2(x):
+        '''
+        FUNCTION TO BE OPTIMISED
+        '''
+        d = len(x)
+        return sum(100 * (x[i + 1] - x[i] ** 2) ** 2 + (x[i] - 1) ** 2 for i in range(d - 1))
+
+    # grad with math rules (by definition)
+    def grad(f, x):
+        '''
+        CENTRAL FINITE DIFFERENCE CALCULATION
+        '''
+        h = np.cbrt(np.finfo(float).eps)
+        d = len(x)
+        nabla = np.zeros(d)
+        for i in range(d):
+            x_for = np.copy(x)
+            x_back = np.copy(x)
+            x_for[i] += h
+            x_back[i] -= h
+            nabla[i] = (f(x_for) - f(x_back)) / (2 * h)
+        return nabla
+
+    grad_f2 = lambda x: grad(f2, x)
+
+    x0 = np.array([-1.2, 1]).T
+    print(bfgs(f2, grad_f2, x0))
